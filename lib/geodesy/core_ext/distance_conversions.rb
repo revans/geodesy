6c6c2eef48776653b2683fd106078e9abf263a28
rrevans <robert@codewranglers.org>
2015-04-24 12:27:42 -0700

    changed how calculations are done

--- lib/geodesy/core_ext/distance_conversions.rb
+++ lib/geodesy/core_ext/distance_conversions.rb
@@ -12,28 +12,188 @@ module Geodesy
       self * 180  / Math::PI
     end
 
-    def to_kilometers(miles)
-      miles * Geodesy::Conversions.miles_to_kilometers
+    # Unit defines what "self" is
+    def to_kilometers(unit)
+      case unit
+      when 'kilometers'
+        self
+
+      when 'miles'
+        self * 0.621371
+
+      when 'meters'
+        self * 1000.0
+
+      when 'yards'
+        self * 1093.61
+
+      when 'feet'
+        self * 3280.84
+
+      when 'inches'
+        self * 39370.1
+
+      when 'centimeters'
+        self * 100000.0
+
+      end
     end
 
-    def to_miles(kilometers)
-      kilometers * Geodesy::Conversions.kilometers_to_miles
+    def to_miles(unit)
+      case unit
+      when 'kilometers'
+        self * 1.60934
+
+      when 'miles'
+        self
+
+      when 'meters'
+        self * 1609.34
+
+      when 'yards'
+        self * 1760.0
+
+      when 'feet'
+        self * 5280.0
+
+      when 'inches'
+        self * 63360.0
+
+      when 'centimeters'
+        self * 160934.0
+
+      end
     end
 
-    def to_meters(yards)
-      yards * Geodesy::Conversions.yards_to_meters
+    def to_meters(unit)
+      case unit
+      when 'kilometers'
+        self * 0.001
+
+      when 'miles'
+        self * 0.000621371
+
+      when 'meters'
+        self
+
+      when 'yards'
+        self * 1.09361
+
+      when 'feet'
+        self * 3.28084
+
+      when 'inches'
+        self * 39.3701
+
+      when 'centimeters'
+        self * 100
+
+      end
     end
 
-    def to_yards(meters)
-      meters * Geodesy::Conversions.meters_to_yards
+    def to_yards(unit)
+      case unit
+      when 'kilometers'
+        self * 0.0009144
+
+      when 'miles'
+        self * 0.000568182
+
+      when 'meters'
+        self * 1760.0
+
+      when 'yards'
+        self
+
+      when 'feet'
+        self * 3
+
+      when 'inches'
+        self * 36
+
+      when 'centimeters'
+        self * 91.44
+
+      end
     end
 
-    def to_feet(yards)
-      yards * Geodesy::Conversions.yards_to_feet
+    def to_feet(unit)
+      case unit
+      when 'kilometers'
+        self * 0.0003048
+
+      when 'miles'
+        self * 0.000189394
+
+      when 'meters'
+        self * 0.3048
+
+      when 'yards'
+        self * 0.333333
+
+      when 'feet'
+        self
+
+      when 'inches'
+        self * 12
+
+      when 'centimeters'
+        self * 30.48
+
+      end
     end
 
-    def to_inches(feet)
-      feet * Geodesy::Conversions.feet_to_inches
+    def to_inches(unit)
+      case unit
+      when 'kilometers'
+        self * 2.54e-5
+
+      when 'miles'
+        self * 1.5783e-5
+
+      when 'meters'
+        self * 0.0254
+
+      when 'yards'
+        self * 0.0277778
+
+      when 'feet'
+        self * 0.0833333
+
+      when 'inches'
+        self
+
+      when 'centimeters'
+        self * 2.54
+
+      end
+    end
+
+
+    def to_centimeters(unit)
+      case unit
+      when 'kilometers'
+        self * 1e-5
+
+      when 'miles'
+        self * 6.2137e-6
+
+      when 'meters'
+        self * 0.01
+
+      when 'yards'
+        self * 0.0109361
+
+      when 'feet'
+        self * 0.0328084
+
+      when 'inches'
+        self * 0.393701
+
+      when 'centimeters'
+        self
+
+      end
     end
 
   end
--- test/fixnum_extension_test.rb
+++ test/fixnum_extension_test.rb
@@ -11,20 +11,32 @@ class Number
     @num.to_radians
   end
 
-  def to_meters(yards)
-    @num.to_meters(yards)
+  def to_kilometers(unit)
+    @num.to_kilometers(unit)
   end
 
-  def to_yards(meters)
-    @num.to_yards(meters)
+  def to_miles(unit)
+    @num.to_miles(unit)
   end
 
-  def to_feet(yards)
-    @num.to_feet(yards)
+  def to_meters(unit)
+    @num.to_meters(unit)
   end
 
-  def to_inches(feet)
-    @num.to_inches(feet)
+  def to_yards(unit)
+    @num.to_yards(unit)
+  end
+
+  def to_feet(unit)
+    @num.to_feet(unit)
+  end
+
+  def to_inches(unit)
+    @num.to_inches(unit)
+  end
+
+  def to_centimeters(unit)
+    @num.to_centimeters(unit)
   end
 end
 
@@ -34,32 +46,181 @@ class FixnumExtensionTest < Minitest::Test
     assert_equal 0.017453292519943295, number.to_radians
   end
 
-  def test_to_meters
-    number  = Number.new(1)
-    expects = 3 * ::Geodesy::YARDS_PER_METER
+  def test_to_kilometers
+    number  = Number.new(2)
 
-    assert_equal expects, number.to_meters(3)
+    expects  = 2 * 100000.0
+    assert_equal expects, number.to_kilometers('centimeters')
+
+    expects  = 2 * 39370.1
+    assert_equal expects, number.to_kilometers('inches')
+
+    expects = 2 * 3280.84
+    assert_equal expects, number.to_kilometers('feet')
+
+    expects = 2 * 1093.61
+    assert_equal expects, number.to_kilometers('yards')
+
+    expects = 2 * 1000.0
+    assert_equal expects, number.to_kilometers('meters')
+
+    expects = 2 * 0.621371
+    assert_equal expects, number.to_kilometers('miles')
+
+    expects = 2
+    assert_equal expects, number.to_kilometers('kilometers')
+  end
+
+
+
+  def test_to_miles
+    number  = Number.new(2)
+
+    expects  = 2 * 160934.0
+    assert_equal expects, number.to_miles('centimeters')
+
+    expects  = 2 * 63360.0
+    assert_equal expects, number.to_miles('inches')
+
+    expects = 2 * 5280.0
+    assert_equal expects, number.to_miles('feet')
+
+    expects = 2 * 1760.0
+    assert_equal expects, number.to_miles('yards')
+
+    expects = 2 * 1609.34
+    assert_equal expects, number.to_miles('meters')
+
+    expects = 2
+    assert_equal expects, number.to_miles('miles')
+
+    expects = 2 * 1.60934
+    assert_equal expects, number.to_miles('kilometers')
+  end
+
+  def test_to_meters
+    number  = Number.new(2)
+
+    expects  = 2 * 100
+    assert_equal expects, number.to_meters('centimeters')
+
+    expects  = 2 * 39.3701
+    assert_equal expects, number.to_meters('inches')
+
+    expects = 2 * 3.28084
+    assert_equal expects, number.to_meters('feet')
+
+    expects = 2 * 1.09361
+    assert_equal expects, number.to_meters('yards')
+
+    expects = 2
+    assert_equal expects, number.to_meters('meters')
+
+    expects = 2 * 0.000621371
+    assert_equal expects, number.to_meters('miles')
+
+    expects = 2 * 0.001
+    assert_equal expects, number.to_meters('kilometers')
   end
 
 
   def test_to_yards
-    number  = Number.new(1)
-    expects = 3 * ::Geodesy::METERS_TO_YARDS
+    number  = Number.new(2)
 
-    assert_equal expects, number.to_yards(3)
+    expects  = 2 * 91.44
+    assert_equal expects, number.to_yards('centimeters')
+
+    expects  = 2 * 36
+    assert_equal expects, number.to_yards('inches')
+
+    expects = 2 * 3
+    assert_equal expects, number.to_yards('feet')
+
+    expects = 2
+    assert_equal expects, number.to_yards('yards')
+
+    expects = 2 * 1760.0
+    assert_equal expects, number.to_yards('meters')
+
+    expects = 2 * 0.000568182
+    assert_equal expects, number.to_yards('miles')
+
+    expects = 2 * 0.0009144
+    assert_equal expects, number.to_yards('kilometers')
   end
 
   def test_to_feet
-    number  = Number.new(1)
-    expects = 3 * 3
+    number  = Number.new(2)
 
-    assert_equal expects, number.to_feet(3)
+    expects  = 2 * 30.48
+    assert_equal expects, number.to_feet('centimeters')
+
+    expects  = 2 * 12
+    assert_equal expects, number.to_feet('inches')
+
+    expects = 2
+    assert_equal expects, number.to_feet('feet')
+
+    expects = 2 * 0.333333
+    assert_equal expects, number.to_feet('yards')
+
+    expects = 2 * 0.3048
+    assert_equal expects, number.to_feet('meters')
+
+    expects = 2 * 0.000189394
+    assert_equal expects, number.to_feet('miles')
+
+    expects = 2 * 0.0003048
+    assert_equal expects, number.to_feet('kilometers')
   end
 
   def test_to_inches
-    number  = Number.new(1)
-    expects = 3 * 12
+    number  = Number.new(2)
 
-    assert_equal expects, number.to_inches(3)
+    expects  = 2 * 2.54
+    assert_equal expects, number.to_inches('centimeters')
+
+    assert_equal 2, number.to_inches('inches')
+
+    expects = 2 * 0.0833333
+    assert_equal expects, number.to_inches('feet')
+
+    expects = 2 * 0.0277778
+    assert_equal expects, number.to_inches('yards')
+
+    expects = 2 * 0.0254
+    assert_equal expects, number.to_inches('meters')
+
+    expects = 2 * 1.5783e-5
+    assert_equal expects, number.to_inches('miles')
+
+    expects = 2 * 2.54e-5
+    assert_equal expects, number.to_inches('kilometers')
   end
+
+
+  def test_to_centimeters
+    number  = Number.new(2)
+
+    assert_equal 2, number.to_centimeters('centimeters')
+
+    expects  = 2 * 0.393701
+    assert_equal expects, number.to_centimeters('inches')
+
+    expects = 2 * 0.0328084
+    assert_equal expects, number.to_centimeters('feet')
+
+    expects = 2 * 0.0109361
+    assert_equal expects, number.to_centimeters('yards')
+
+    expects = 2 * 0.01
+    assert_equal expects, number.to_centimeters('meters')
+
+    expects = 2 * 6.2137e-6
+    assert_equal expects, number.to_centimeters('miles')
+
+    expects = 2 * 1e-5
+    assert_equal expects, number.to_centimeters('kilometers')
+  end
+
 end
